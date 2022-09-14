e20862b36630354ca3221d608d52c4973d945101
Maqsud Alam <97883113+maqsud8585@users.noreply.github.com>
2022-09-15 02:42:42 +0530

    Add files via upload

---
+++ practice-question-1.js
@@ -0,0 +1,76 @@
+/* 1. return the sum of two numbers */
+// function addition(num1,num2) {
+//     return num1+num2;
+// }
+
+// console.log(addition(5,4)); 
+
+/*2. Convert minutes into seconds */
+// function convert (a) {
+//     return a*60;
+// }
+
+// console.log(convert(3));
+
+/* 3. Return the next number from the integer passed */
+// function addition(a) {
+//     return a+=1;
+// }
+
+// console.log(addition(0));
+
+/* 4.find Area of triangle */
+// function triArea(base,height) {
+//     area=1/2*base*height ;
+//     return area;
+// }
+// console.log(triArea(10,10));
+
+/* 5. convert age to days */
+// function calcAge(age) {
+//     return age*365;    
+// }
+
+// console.log(calcAge(20));
+
+/* 6.Return the first element in an array */
+// function getFirstValue(arr) {
+//     b=arr.slice(0,1);
+//     return b; 
+// }
+// console.log(getFirstValue([80,5,100]));
+
+// second method 
+// function getFirstValue(arr) {
+//     return arr[0];
+// }
+
+// console.log(getFirstValue([80,5,100]));
+
+/* 7.Power calculater */
+// function circuitPower(voltage,current) {
+//     power=voltage*current;
+//     return power;
+// }
+// console.log(circuitPower(110,3));
+
+/* 8. Convert hours into seconds */
+// function howManySeconds(hour) {
+//     return hour*3600;
+// }
+// console.log(howManySeconds(2));
+
+/* 9.Return the remainder from two numbers */
+// function remainder(a,b) {
+// return a % b;    
+// }
+// console.log(remainder(5,5));
+
+/* 10. Find perimeter of rectangle */
+// function findPerimeter(a,b) {
+// c = 2 * (a + b) ;
+// return c ;    
+// }
+// console.log(findPerimeter(6,7));
+
+
