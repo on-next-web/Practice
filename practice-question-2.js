ace3f5df6b6e0fa81ab1cb060cb744927d02952f
Maqsud Alam <97883113+maqsud8585@users.noreply.github.com>
2022-09-19 11:30:06 +0530

    Add files via upload

---
+++ practice-question-2.js
@@ -0,0 +1,114 @@
+/* is the number less than or equal to zero */
+// function lessThanOrEqualToZero(a){
+//     if (a<=0) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }   
+// }
+
+// console.log(lessThanOrEqualToZero(8));
+
+/* Less than 100 */
+
+// function lessThan100(a,b){
+//     if ((a+b)<100) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+// console.log(lessThan100(20,40));
+
+/* swap two numbers */
+
+// function swap(a,b) {
+//     let c,d;
+//     c = a;
+//     a = b;
+//     b = c;
+//     return [a,b];
+// }
+// console.log(swap(5,6));
+
+/* the farm problem */
+
+// function animals(chics,cows,pigs) {
+//     let total;
+//     chics = chics*2;
+//     cows = cows*4;
+//     pigs = pigs*4;
+//     total = chics+cows+pigs;
+//     return total;
+// }
+
+// console.log(animals(1,2,3));
+
+/* logical operator && */
+
+// function and(a,b){
+//     if ((a=== true)&&(b=== true)) {
+//      return true;   
+//     }
+//     else {
+//         return false;
+//     }
+// }
+// console.log(and(true,true));
+
+/* Are the n numbers equals */
+
+// function isSameNum(a,b){
+//     if (a===b) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+
+// }
+//  console.log(isSameNum(2,"2"));
+
+/* football points */
+
+// function footballPoints(a,b,c){
+//     let d;
+//     a=a*3;
+//     b=b*1;
+//     c=c*0;
+//     d=a+b+c;
+//     return d;
+// }
+
+// console.log(footballPoints(2,4,2));
+// console.log(footballPoints(5,0,2));
+
+/* Check equality */
+
+// function checkEquality(a,b){
+//     if (a===b) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+// console.log(checkEquality(1,1));
+
+/* Profitable Gamble */
+
+// function profitableGamble(a,b,c){
+//     if ((a*b)>c) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+// console.log(profitableGamble(0.9,3,2));
+
