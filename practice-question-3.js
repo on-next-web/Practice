4d967474244efbe97accda7c9d3d2b31a8c4effe
Maqsud Alam <97883113+maqsud8585@users.noreply.github.com>
2022-09-24 14:05:37 +0530

    Add files via upload

---
+++ practice-question-3.js
@@ -0,0 +1,107 @@
+/*1. write a function that returns a greeting to user */
+
+// function greeting(name) {
+//   let greet = "hello " + name;
+//   return greet;
+// }
+
+// console.log(greeting("Akhter"));
+
+/*2. create a function that takes two arguments both arguments are integers
+ a and b return true if one of them is 10 or if their sum is 10. */
+
+// function makesTen(a,b){
+//     if (a==10 || b==10) {
+//         return true;
+//     } else if ((a+b)==10){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// console.log(makesTen(5,5));
+
+/*3. Return true if a string is empty and false otherwise */
+
+// function isEmpty(str) {
+//   if (str===("")) {
+//     return true;
+//   }  else {
+//     return false ;
+//   }
+// }
+
+// console.log(isEmpty(""));
+
+/*4. integer is evenly divisible by 5 or not */
+
+// function divisibleByFive(num){
+//     if (num%5==0){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+// console.log(divisibleByFive(5));
+
+/*5. returns the length of a string */
+
+// function length(str) {
+//     result = str.length;
+//     return result;
+// }
+
+// console.log(length('sajida'));
+// console.log(length('max'));
+
+/*6. calculates the area of a rectangle */
+
+// function area(length,width){
+//     a = length*width;
+//     if ((length>0)&&(width>0)){
+//         return a;
+//     } else {
+//         return -1 ;
+//     }
+// }
+
+// console.log(area(0,1));
+
+/*7. function to reverse an array */
+
+// function reverse(arr){
+//     let change= arr.reverse();
+//     return change;
+// }
+
+// console.log(reverse([1,2,3,4]));
+
+/*8. increment of 1 to every element of array */
+
+// function incrementItems(a){
+//     let arr=[];
+//     for(let i=0; i<a.length; i++){
+//         arr.push(a[i]+1)
+//     }
+//     return arr;
+// }
+// console.log(incrementItems([0, 1, 2, 3]));
+
+/*9. template string according to example */
+
+
+// const a="Akhter";
+// const b="Sajida";
+// const c="Max";
+
+// const template=`${a}, ${b} and ${c}`;
+// console.log(template);
+
+/*10. A bartender is writing a simple program to determine whether he should serve drinks to someone.
+ He only serves drinks to people 18 and older and when he's not on break. */
+
+ 
